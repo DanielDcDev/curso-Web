07467126c41ca9c62eedf4d6d2d7559ba3038ba5
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2023-10-30 12:57:13 +0000

    updateCurso

---
+++ javascript/pratica/script/arrayD.js
@@ -0,0 +1,11 @@
+var lista_coisas = Array();
+
+lista_coisas['frutas'] = Array('manga','banana', 'Maca')
+
+lista_coisas['pessoas'] = []
+lista_coisas['pessoas']['a'] =  'Caio'
+lista_coisas['pessoas']['b'] =  'Matheus'
+lista_coisas['pessoas']['c'] =  'Daniel'
+
+document.write(lista_coisas['frutas'][2])
+document.write(lista_coisas['pessoas']['c'])
