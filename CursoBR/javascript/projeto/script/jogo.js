ceeaeae3427360f20ce7ed9e51eaac5be9a29a74
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2023-11-05 20:22:25 +0000

    update day 5/11

--- CursoBR/javascript/projeto/script/jogo.js
+++ CursoBR/javascript/projeto/script/jogo.js
@@ -1,6 +1,31 @@
 var altura = 0;
 var largura = 0;
-let vidas = 1
+let vidas = 1;
+let tempo = 10;
+var criaMosca = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?','')
+if(nivel === 'normal'){
+    criaMosca=2000
+}else if(nivel === 'dificil'){
+    criaMosca = 1500
+}else if(nivel === 'chucknorris'){
+    criaMosca = 750
+}
+let cronometro = setInterval(function(){
+    tempo -= 1
+    document.getElementById('cronometro').innerHTML = tempo
+    
+    if(tempo < 0) {
+		clearInterval(cronometro)
+		clearInterval(criaMosca)
+
+        window.location.href = 'vitoria.html';
+	} else {
+		document.getElementById('cronometro').innerHTML = tempo
+	}
+},1000)
 
 function ajustaTamanho(){
     var altura = window.innerHeight;
@@ -12,6 +37,8 @@ function randomicPosition(){
     if (document.getElementById('mosquito')){
         document.getElementById('mosquito').remove()
         if(vidas >3){
+            window.location.href = 'fimDeJogo.html';
+        }else {
             document.getElementById('v'+ vidas).src ="../img/coracao_vazio.png"
             vidas++
         }
